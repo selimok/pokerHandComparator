965dcb92157db5da03a75902e372d497827ab80d
selimok <selimok@bearge.com>
2015-03-22 16:09:27 +0100

    Improve UI: Deal out cards randomly and show the cards as images.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,120 +1,71 @@
-var pokerHands = [];
+var pokerHands;
+var pokerDeck;
 var suits = ["HEARTS", "CLUBS", "DIAMONDS", "SPADES"];
 var values = ["ACE","TWO", "THREE", "FOUR", "FIVE", "SIX",
               "SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"];
 
-function PokerHand(handId) {
-	this.handId = handId;
-	this.cards = [{
-						"value": "ACE",
-						"suit": "HEARTS"
-					},
-					{
-						"value": "ACE",
-						"suit": "HEARTS"
-					},
-					{
-						"value": "ACE",
-						"suit": "HEARTS"
-					},
-					{
-						"value": "ACE",
-						"suit": "HEARTS"
-					},
-					{
-						"value": "ACE",
-						"suit": "HEARTS"
-					}];
-	
+function initPokerDeck(){
+	pokerDeck = [];
+	for(i = 0; i < suits.length ; i++){
+		for(j = 0; j < values.length ; j++){
+			pokerDeck.push(new PokerCard(suits[i], values[j]))
+		}
+	}
 }
 
-function createPokerCard(suit,value) {
+function PokerHand(handId) {
+	this.handId = handId;
+	this.cards = [];
+}
+
+function PokerCard(suit,value) {
 	this.suit = suit;
 	this.value = value;
 }
 
-function addPokerHandPanel() {
-	var numberOfPokerHands = pokerHands.length;
-	if(numberOfPokerHands < 10){
-		divId = 'poker-hand-' + numberOfPokerHands;
-		$("#poker-hand-panel" ).append('<div id="' + divId + '" class="poker-hand"></div>');
-		fillPokerHandPanel(divId, numberOfPokerHands);		
-	} else {
-		alert("Maximum number of poker hands reached. Refresh page if you want add new poker hand.");
-
+function dealOutCardsRandomly() {
+	initPokerDeck();
+	pokerHands = [];
+	console.log( "Poker deck is initialized with " + pokerDeck.length + " cards.");
+	
+	pokerHandOne = new PokerHand(1);
+	pokerHandTwo = new PokerHand(2);
+	
+	for(i = 0; i < 5 ; i++){
+		pokerHandOne.cards.push(pickACardRandomly());
+		pokerHandTwo.cards.push(pickACardRandomly());
 	}
 	
+	pokerHands.push(pokerHandOne);
+	pokerHands.push(pokerHandTwo);
+	
+	console.log( "Poker cards dealed out randomly.");
+	console.log( "Ready to compare!" );
+	
+	updateGUI();
 }
 
-function fillPokerHandPanel(divId, handId) {
-		$("#"+divId )
-		.append('<div class="handId">Poker Hand: '+ handId + '</div>')
-		.append('<div class="rank" id="rank-' + handId + '"></div>');
-		
-		pokerHands.push(new PokerHand(handId));
-		
-		for(i = 0; i < 5; i++){
-			
-			cardDiv = $("<div/>")
-			.attr("class","card")
-			.append(createSuitSelectBox(handId, i))
-			.append(createValueSelectBox(handId, i));
-			
-			$("#"+divId ).append(cardDiv);
+function pickACardRandomly() {
+	randomCardIndex = Math.floor(Math.random()*pokerDeck.length);
+	randomCard = pokerDeck[randomCardIndex];
+	pokerDeck.splice(randomCardIndex, 1);
+	return randomCard;
+}
+
+function updateGUI(){
+	
+	for(i = 0; i < pokerHands.length ; i++){
+		pokerHand = pokerHands[i];
+		handId = pokerHand.handId;
+		pokerHandDiv = $("#poker-hand-" + handId);
+		pokerHandInfoDiv = $("#poker-hand-" + handId + "-info");
+		pokerHandDiv.empty();
+		pokerHandInfoDiv.empty();
+		for(j = 0; j < 5 ; j++){
+			pokerCard = pokerHand.cards[j];
+			pokerHandDiv.append('<div class="poker-card"><img src="img/' + pokerCard.value + '-' + pokerCard.suit +'.png" title="' + pokerCard.value + '-' + pokerCard.suit +'"></div>');
 		}
-}
-
-function createSuitSelectBox(handId, cardIndex) {
-	var selectBoxId = "suitSelect-"+handId+"-"+cardIndex;
-	var selectBox = $("<select/>")
-	.attr("class", "suit-select")
-	.attr("id", selectBoxId)
-    .attr("handId", handId)
-    .attr("cardIndex", cardIndex)
-	.on('change', function() { updateCardSuit(selectBoxId) });
-	
-	$(suits).each(function() {
-		selectBox.append($("<option>").attr('value',this).text(this));
-	});
-	return selectBox;
-}
-
-function createValueSelectBox(handId, cardIndex) {
-	var selectBoxId = "valueSelect-"+handId+"-"+cardIndex;
-	var selectBox = $("<select/>")
-	.attr("class", "value-select")
-	.attr("id", selectBoxId)
-    .attr("handId", handId)
-    .attr("cardIndex", cardIndex)
-    .on('change', function() { updateCardValue(selectBoxId) });
-	
-	$(values).each(function() {
-		selectBox.append($("<option>").attr('value',this).text(this));
-	});
-	return selectBox;
-}
-
-function updateCardValue(selectBoxId){
-	var selectBox = $("#"+selectBoxId);
-	var pokerHand = getPokerHandById(selectBox.attr("handId"));
-	pokerHand.cards[selectBox.attr("cardIndex")].value = selectBox.val();
-}
-
-function updateCardSuit(selectBoxId){
-	var selectBox = $("#"+selectBoxId);
-	var pokerHand = getPokerHandById(selectBox.attr("handId"));
-	pokerHand.cards[selectBox.attr("cardIndex")].suit = selectBox.val();
-}
-
-function getPokerHandById(handId){
-	var pokerHand = null;
-	for(i = 0; i < pokerHands.length; i++){
-		if (pokerHands[i].handId == handId) {
-			return pokerHands[i];
-		} 
 	}
-	
-	return pokerHand;
 }
 
 function comparePokerHands(){
@@ -133,18 +84,24 @@ function handleCompareResults(response){
 	
 	for (i = 0; i < response.length; i++){
 		var handId = response[i].handId;
-		var rank = response[i].rank;
+		var rank = response[i].rank.replace(/_/g,' ');
 		var overallRank = response[i].overallRank;
-		$('#rank-'+handId).empty().append("Overall Rank: " + overallRank + " / Rank: " + rank);
+		
+		pokerHandInfoDiv = $("#poker-hand-" + handId + "-info");
+		
+		pokerHandInfoDiv.empty();
+		pokerHandInfoDiv.append('<span class="poker-hand-rank">'+rank+'</span><br/>');
+		
 		if(overallRank == 1){
-			$('#rank-'+response[i].handId).append(" / WINNER! ");
+			pokerHandInfoDiv.append('<span class="poker-hand-winner">Winner</span>');
 		}
+		
 	}
 	
 }
 
-$("#addPokerHandButton" ).click(function() {
-	addPokerHandPanel();
+$("#dealButton" ).click(function() {
+	dealOutCardsRandomly();
 });
 
 $("#compareButton" ).click(function() {
@@ -154,7 +111,5 @@ $("#compareButton" ).click(function() {
 
 
 $(document).ready(function() {
-	initPokerDeck();
-	console.log( "Poker deck is initialized with " + pokerDeck.length + " cards.");
-    console.log( "Ready to compare!" );
+	
 });
